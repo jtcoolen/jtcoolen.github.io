9e4f0bc7236df758518038d9e6304e70f0589ad6
Julien Coolen <jtcoolen@pm.me>
2024-12-13 03:01:02 +0100

    fix dates

--- quartz.config.ts
+++ quartz.config.ts
@@ -51,6 +51,7 @@ const config: QuartzConfig = {
         // you can add 'git' here for last modified from Git
         // if you do rely on git for dates, ensure defaultDateType is 'modified'
         priority: ["frontmatter", "filesystem"],
+	defaultDateType: ["modified"],
       }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.SyntaxHighlighting(),
